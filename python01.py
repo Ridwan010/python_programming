386886089cd2e82188aee6b4ae314c5a8f0d7f24
Azeez Ridwan <azeezridwan57@gmail.com>
2022-10-09 12:27:26 +0100

    python programming

---
+++ python01.py
@@ -0,0 +1,7 @@
+supplies=10
+discounts=30/100
+state_tax=5/100
+shipping_fee=7.50
+
+print("Total_price = (supplies-(supplies*discounts))+(supplies+(supplies*state_tax))+(shipping_fee)")
+print("Total_price =",(supplies-(supplies*discounts))+(supplies+(supplies*state_tax))+(shipping_fee))
