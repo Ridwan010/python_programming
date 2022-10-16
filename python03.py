4510b7150b9c8f4354118bdeaa43e5e7f85e6248
Azeez Ridwan <azeezridwan57@gmail.com>
2022-10-16 16:56:24 +0100

    python programming

---
+++ python03.py
@@ -0,0 +1,10 @@
+# A snippet of code that will print “You have money” if the number is positive, “You’re out” if it’s zero, and “You seem to be in debt” if it's less than zero
+
+a = -3
+if a >1:
+    print("You have money")
+elif a == 0:
+    print("You are out")
+else:
+    print("You are in debt")
+
